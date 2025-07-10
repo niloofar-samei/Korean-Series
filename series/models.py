c5141a14441df09226f2ffc69c2b76ed44ded87d
niloofar-samei <niloofar.samei@gmail.com>
2025-07-10 14:15:20 +0000

    Create Movie model.

--- series/models.py
+++ series/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=200)
+    released_year = models.IntegerField(max_length=200)
+
+    def __str__(self):
+        return self.movie_name

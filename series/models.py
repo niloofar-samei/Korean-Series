d2cb988a6f7b8733df6fa6aca252728e9298c3fe
niloofar-samei <niloofar.samei@gmail.com>
2025-07-10 14:25:24 +0000

    Bug fix.

--- series/models.py
+++ series/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     movie_name = models.CharField(max_length=200)
-    released_year = models.IntegerField(max_length=200)
+    released_year = models.IntegerField(default=0)
 
     def __str__(self):
         return self.movie_name

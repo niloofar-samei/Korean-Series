44953235a37f91e4da3fedd94f01b273bd6e735f
niloofar-samei <niloofar.samei@gmail.com>
2025-07-24 15:44:00 +0000

    Register Actress in admin.

--- series/admin.py
+++ series/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Movie, Actor
+from models import Movie, Actor, Actress
 
 admin.site.register(Movie)
 admin.site.register(admin)
+admin.site.register(Actress)

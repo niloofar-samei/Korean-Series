6c9511a36b67c77b96624e357577e83d538f3ca4
niloofar-samei <niloofar.samei@gmail.com>
2025-07-24 21:34:20 +0000

    Bug fix.

--- series/admin.py
+++ series/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from models import Movie, Actor, Actress
 
 admin.site.register(Movie)
-admin.site.register(admin)
+admin.site.register(Actor)
 admin.site.register(Actress)

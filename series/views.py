e629bd170fa9163e4d1c724be74bf70d52cb38e4
niloofar-samei <niloofar.samei@gmail.com>
2025-07-25 21:48:50 +0100

    Create def movie to show movie in an individual page.

--- series/views.py
+++ series/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
 def index(request):
     movie_list = Movie.objects.all()
     return render(request, "series/index.html", {"movie_list": movie_list})
+
+
+def movie(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, "series/movie.html", {"movie": movie})

6e1373e509bd58f459102681c719d3101757e62a
niloofar-samei <niloofar.samei@gmail.com>
2025-07-26 00:04:39 +0100

    Add css to movie.html.

--- series/views.py
+++ series/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Movie
+from .models import Movie, Actor, Actress
 
 
 def index(request):
@@ -10,4 +10,12 @@ def index(request):
 
 def movie(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
-    return render(request, "series/movie.html", {"movie": movie})
+    actor = movie.actor_set.all()
+    actress = movie.actress_set.all()
+    print(actor)
+    print(actress)
+    return render(
+        request,
+        "series/movie.html",
+        {"movie": movie, "actor": actor, "actress": actress},
+    )

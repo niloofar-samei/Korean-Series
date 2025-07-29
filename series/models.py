a42971bb6bb3cce84b006fdd5352fe449c783974
niloofar-samei <niloofar.samei@gmail.com>
2025-07-29 16:47:23 +0100

    Add voteup to Movie model.

--- series/models.py
+++ series/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Movie(models.Model):
     movie_name = models.CharField(max_length=200)
     released_year = models.IntegerField(default=0)
+    voteup = models.IntegerField(default=0)
 
     def __str__(self):
         return self.movie_name

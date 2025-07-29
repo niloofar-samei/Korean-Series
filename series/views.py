39e1c79ccf230edd9d35bf44252b80db92b9bce0
niloofar-samei <niloofar.samei@gmail.com>
2025-07-29 08:17:25 +0100

    Make def new working and preventing same records.

--- series/views.py
+++ series/views.py
@@ -21,39 +21,24 @@ def movie(request, movie_id):
 
 def new(request):
     if request.method == "POST":
-        the_movie = Movie.objects.get(movie_name=request.POST.get("movie"))
-        if not the_movie:
+        existing_movie = Movie.objects.filter(
+            movie_name=request.POST.get("movie")
+        ).first()
+        print(existing_movie)
+        if existing_movie:
+            print("again?")
+            return render(request, "series/new.html")
+        else:
+            print("its new.")
             new_movie = request.POST.get("movie")
             new_year = request.POST.get("year")
             actress_name = request.POST.get("actress")
             actor_name = request.POST.get("actor")
-            Movie.objects.create(movie_name=new_movie, released_year=new_year)
-            selected_movie = Movie.objects.get(movie_name=new_movie)
-            selected_movie.actress_set.create(actress_name=actress_name)
-            selected_movie.actor_set.create(actor_name=actor_name)
+            new_movie = Movie.objects.create(
+                movie_name=new_movie, released_year=new_year
+            )
+            new_movie.actress_set.create(actress_name=actress_name)
+            new_movie.actor_set.create(actor_name=actor_name)
             return render(request, "series/new.html")
-        else:
-            print("we had it")
-            return render(request, "series/new.html")
-    if request.method == "GET":
-        return render(request, "series/new.html")
 
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    return render(request, "series/new.html")

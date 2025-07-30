da4e478b509a0aa69a80987513b371e634f8aa73
niloofar-samei <niloofar.samei@gmail.com>
2025-07-30 20:10:04 +0100

    Bug fix.

--- series/models.py
+++ series/models.py
@@ -5,8 +5,8 @@ class Movie(models.Model):
     movie_name = models.CharField(max_length=200, unique=True)
     released_year = models.IntegerField(default=0)
     voteup = models.IntegerField(default=0)
-    movie_image = models.ImageField(upload_to="movie/")
-    movie_about = models.CharField(max_length=500)
+    movie_image = models.ImageField(upload_to="movie/", blank=True, null=True)
+    movie_about = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.movie_name
@@ -15,7 +15,7 @@ class Movie(models.Model):
 class Actor(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     actor_name = models.CharField(max_length=200)
-    actor_image = models.ImageField(upload_to="actor/")
+    actor_image = models.ImageField(upload_to="actor/", blank=True, null=True)
 
     def __str__(self):
         return self.actor_name
@@ -24,7 +24,7 @@ class Actor(models.Model):
 class Actress(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     actress_name = models.CharField(max_length=200)
-    actress_image = models.ImageField(upload_to="actress/")
+    actress_image = models.ImageField(upload_to="actress/", blank=True, null=True)
 
     def __str__(self):
         return self.actress_name

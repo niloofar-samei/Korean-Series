961c31ed2d6b8b7071add652d2eb087f2feb73bc
niloofar-samei <niloofar.samei@gmail.com>
2025-07-31 19:49:19 +0100

    Remove their urls.

--- series/urls.py
+++ series/urls.py
@@ -6,16 +6,6 @@ urlpatterns = [
     path("movie/<int:movie_id>/", views.movie, name="movie"),
     path("new/", views.new, name="new"),
     path("new_django_form", views.new_django_form, name="new_django_form"),
-    path(
-        "new_django_form_movie/",
-        views.new_django_form_movie,
-        name="new_django_form_movie",
-    ),
-    path(
-        "add_actress-actor/<int:movie_id>/",
-        views.add_actress_actor,
-        name="add_actress_actor",
-    ),
     path("delete/<int:movie_id>", views.delete, name="delete"),
     path("voteup/<int:movie_id>", views.voteup, name="voteup"),
 ]
